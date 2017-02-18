d74704bb97d97ea15ea72d1b87f1f0310b8d8abe
Paul Maigutyak <pmaigutyak@ettera.com>
2017-02-18 13:35:46 +0200

    Add download url.

--- setup.py
+++ setup.py
@@ -6,13 +6,14 @@ except ImportError:
 
 
 setup(
-    name='mp-seo',
-    version='1.0.0',
+    name='django-mp-seo',
+    version='1.0',
     description='Django seo app',
     long_description=open('README.md').read(),
     author='Paul Maigutyak',
     author_email='pmaigutyak@gmail.com',
     url='https://github.com/pmaigutyak/mp-seo',
+    download_url='https://github.com/pmaigutyak/mp-seo/archive/1.0.tar.gz',
     packages=['seo'],
     license='MIT',
     install_requires=[
